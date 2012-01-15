b60bc718f16a57a9aeec629f5586e86bb818aa89
mbakhterev <mob@k.imm.uran.ru>
2012-01-15 21:57:45 +0600

    clean abstracted directory checking and making

--- common.mk
+++ common.mk
@@ -22,41 +22,28 @@ endif
 
 c2o = $(addprefix $(1)/, $(patsubst %.c, %.o, $(2)))
 o2d = $(patsubst %.o, %.d, $(1))
+
 mkpath = \
 	{ test -d '$(1)' \
  		|| { echo 'error: no build dir: $(1)' 1>&2; false; }; } \
  	&& { test -d $(2) || mkdir -p '$(2)'; }
 
-$(bld)/%.d: ./%.c
-# 	@ echo -e '\tdep\t$<' \
-# 	&& { test -d '$(bld)' \
-# 		|| { echo 'error: no build dir: $(bld)' 1>&2; false; }; } \
-# 	&& { test -d $(@D) || mkdir -p '$(@D)'; } \
-# 	&& $(cc) $(cflags) -MM $< \
-# 		| awk '{ gsub("$(*F).o", "$(@D)/$(*F).o $@"); print }' > $@
+$(bld)/%.d: %.c
 	@ echo -e '\tdep\t$<' \
 	&& $(call mkpath,$(bld),$(@D)) \
  	&& $(cc) $(cflags) -MM $< \
  		| awk '{ gsub("$(*F).o", "$(@D)/$(*F).o $@"); print }' > $@
  
-
-$(bld)/%.o: ./%.c
+$(bld)/%.o: %.c
 	@ echo -e '\tcc\t$<' \
 	&& $(cc) $(cflags) $(copt) -o $@ $<
 
 $(bld)/lib/%.a:
 	@ echo -e '\tlib\t$@' \
-	&& { test -d '$(bld)' \
-		|| { echo 'error: no build dir: $(bld)' 1>&2; false; }; } \
-	&& { test -d $(@D) || mkdir -p '$(@D)'; } \
+	&& $(call mkpath,$(bld),$(@D)) \
 	&& $(ar) rc $@ $^
 
 $(bld)/bin/%:
 	@ echo -e '\tlnk\t$@' \
-	&& { test -d '$(bld)' \
-		|| { echo 'error: no build dir: $(bld)' 1>&2; false; }; } \
-	&& { test -d $(@D) || mkdir -p '$(@D)'; } \
+	&& $(call mkpath,$(bld),$(@D)) \
 	&& $(lnk) $(lflags) $^ -o $@
-
-# c2o = $(addprefix $(1)/, $(patsubst %.c, %.o, $(2)))
-# o2d = $(patsubst %.o, %.d, $(1))
