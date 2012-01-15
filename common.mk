2a678cece7b8b66ae5467fcc5212411dd68b4810
mbakhterev <mob@k.imm.uran.ru>
2012-01-15 16:51:55 +0600

    abstraction of shell code to insure some directories exist. experimental dirty variant

--- common.mk
+++ common.mk
@@ -20,13 +20,25 @@ copt = $(coptimization)
 lflags += $(loptimization)
 endif
 
+c2o = $(addprefix $(1)/, $(patsubst %.c, %.o, $(2)))
+o2d = $(patsubst %.o, %.d, $(1))
+mkpath = \
+	{ test -d '$(1)' \
+ 		|| { echo 'error: no build dir: $(1)' 1>&2; false; }; } \
+ 	&& { test -d $(2) || mkdir -p '$(2)'; }
+
 $(bld)/%.d: ./%.c
+# 	@ echo -e '\tdep\t$<' \
+# 	&& { test -d '$(bld)' \
+# 		|| { echo 'error: no build dir: $(bld)' 1>&2; false; }; } \
+# 	&& { test -d $(@D) || mkdir -p '$(@D)'; } \
+# 	&& $(cc) $(cflags) -MM $< \
+# 		| awk '{ gsub("$(*F).o", "$(@D)/$(*F).o $@"); print }' > $@
 	@ echo -e '\tdep\t$<' \
-	&& { test -d '$(bld)' \
-		|| { echo 'error: no build dir: $(bld)' 1>&2; false; }; } \
-	&& { test -d $(@D) || mkdir -p '$(@D)'; } \
-	&& $(cc) $(cflags) -MM $< \
-		| awk '{ gsub("$(*F).o", "$(@D)/$(*F).o $@"); print }' > $@
+	&& $(call mkpath,$(bld),$(@D)) \
+ 	&& $(cc) $(cflags) -MM $< \
+ 		| awk '{ gsub("$(*F).o", "$(@D)/$(*F).o $@"); print }' > $@
+ 
 
 $(bld)/%.o: ./%.c
 	@ echo -e '\tcc\t$<' \
@@ -46,5 +58,5 @@ $(bld)/bin/%:
 	&& { test -d $(@D) || mkdir -p '$(@D)'; } \
 	&& $(lnk) $(lflags) $^ -o $@
 
-c2o = $(addprefix $(1)/, $(patsubst %.c, %.o, $(2)))
-o2d = $(patsubst %.o, %.d, $(1))
+# c2o = $(addprefix $(1)/, $(patsubst %.c, %.o, $(2)))
+# o2d = $(patsubst %.o, %.d, $(1))
