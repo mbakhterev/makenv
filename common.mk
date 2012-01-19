f9356542013e61f2b240f24efb5626f3b3cb520c
mbakhterev <mob@k.imm.uran.ru>
2012-01-19 21:27:39 +0600

    A little of beautification

--- common.mk
+++ common.mk
@@ -28,16 +28,6 @@ mkpath = \
  		|| { echo 'error: no build dir: $(1)' 1>&2; false; }; } \
  	&& { test -d $(2) || mkdir -p '$(2)'; }
 
-$(bld)/%.d: %.c
-	@ echo -e '\tdep\t$<' \
-	&& $(call mkpath,$(bld),$(@D)) \
- 	&& $(cc) $(cflags) -MM $< \
- 		| awk '{ gsub("$(*F).o", "$(@D)/$(*F).o $@"); print }' > $@
- 
-$(bld)/%.o: %.c
-	@ echo -e '\tcc\t$<' \
-	&& $(cc) $(cflags) $(copt) -o $@ $<
-
 $(bld)/lib/%.a:
 	@ echo -e '\tlib\t$@' \
 	&& $(call mkpath,$(bld),$(@D)) \
@@ -47,3 +37,13 @@ $(bld)/bin/%:
 	@ echo -e '\tlnk\t$@' \
 	&& $(call mkpath,$(bld),$(@D)) \
 	&& $(lnk) $(lflags) $^ -o $@
+
+$(bld)/%.d: %.c
+	@ echo -e '\tdep\t$<' \
+	&& $(call mkpath,$(bld),$(@D)) \
+ 	&& $(cc) $(cflags) -MM $< \
+ 		| awk '{ gsub("$(*F).o", "$(@D)/$(*F).o $@"); print }' > $@
+ 
+$(bld)/%.o: %.c
+	@ echo -e '\tcc\t$<' \
+	&& $(cc) $(cflags) $(copt) -o $@ $<
