d21715e40c37cb6928095055a9f13b543a6a1283
mbakhterev <mob@k.imm.uran.ru>
2012-04-01 00:06:53 +0600

    Minor refinements (or not, but minor)

--- gnu/common.mk
+++ gnu/common.mk
@@ -22,8 +22,8 @@ lflags += $(loptimization)
 endif
 
 o2d = $(patsubst %.o,%.d,$(1))
-c2o = $(addprefix $(1), $(patsubst %.c,%.o,$(2)))
-cpp2o = $(addprefix $(1), $(patsubst %.cpp,%.o,$(2)))
+c2o = $(addprefix $(1)/,$(patsubst %.c,%.o,$(2)))
+cpp2o = $(addprefix $(1)/,$(patsubst %.cpp,%.o,$(2)))
 
 mkpath = \
 	{ test -d '$(1)' \
@@ -32,53 +32,57 @@ mkpath = \
 
 
 bits = $(bld)/B
-bitspath = $(bits)/$(dir $(lastword $(MAKEFILE_LIST)))
+bitspath = $(bits)/$(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
+
+B = $(bld)/bin
+L = $(bld)/lib
+I = $(bld)/include
 
 $(bits)/%.d: %.c
-	@ echo -e '\tdep\t$<' \
+	@ echo -e '\tdep\t$@' \
 	&& $(call mkpath,$(bld),$(@D)) \
  	&& $(cc) $(cflags) -x c -std=$(cstd) -MM $< \
  		| awk '{ gsub("$(*F).o", "$(@D)/$(*F).o $@"); print }' > $@
 
 $(bits)/%.d: %.cpp
-	@ echo -e '\tdep c++\t$<' \
+	@ echo -e '\tdep c++\t$@' \
 	&& $(call mkpath,$(bld),$(@D)) \
  	&& $(cc) $(cflags) -x c++ -std=$(cppstd) -MM $< \
  		| awk '{ gsub("$(*F).o", "$(@D)/$(*F).o $@"); print }' > $@
  
 $(bits)/%.o: %.c
-	@ echo -e '\tcc\t$<' \
+	@ echo -e '\tcc\t$@' \
 	&& $(cc) $(cflags) $(copt) -x c -std=$(cstd) -o $@ $<
  
 $(bits)/%.o: %.cpp
-	@ echo -e '\tcc c++\t$<' \
+	@ echo -e '\tcc c++\t$@' \
 	&& $(cc) $(cflags) $(copt) -x c++ -std=$(cppstd) -o $@ $<
 
-$(bld)/bin/%: %.sh
+$(B)/%: %.sh
 	@ echo -e '\tinstall\t$@' \
 	&& $(call mkpath,$(bld),$(@D)) \
 	&& install -m 755 $< $@
 
-$(bld)/bin/%:
+$(B)/%:
 	@ echo -e '\tlink\t$@' \
 	&& $(call mkpath,$(bld),$(@D)) \
-	&& $(lnk) $(lflags) $^ -o $@
+	&& $(lnk) $^ -o $@ $(lflags) 
 
-$(bld)/lib/%.a:
-	@ echo -e '\tlib\t$<' \
+$(L)/%.a:
+	@ echo -e '\tlib\t$@' \
 	&& $(call mkpath,$(bld),$(@D)) \
-	&& $(ar) r $@ $^
+	&& $(ar) rc $@ $^
 
-$(bld)/include/%.h:
+$(I)/%.h:
 	@ echo -e '\theader\t$@' \
 	&& $(call mkpath,$(bld),$(@D)) \
 	&& install -m 755 $< $@
 
 $(bld)/%.pdf: $(bld)/%.tex
-	@ echo -e '\ttex\t$<' \
+	@ echo -e '\ttex\t$@' \
 	&& (cd $(@D) && ($(tex) $< && $(tex) $<) | iconv -f $(texcode))
 
 $(bld)/%.tex: %.tex
-	@ echo -e '\tcp\t$<' \
+	@ echo -e '\tcp\t$@' \
 	&& $(call mkpath,$(bld),$(@D)) \
 	&& iconv -t $(texcode) < $< > $@
