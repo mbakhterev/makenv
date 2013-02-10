1590b96d4771708f53a4af55f072253b10323f0a
mbakhterev <mob@k.imm.uran.ru>
2013-02-10 17:44:59 +0600

    More universal Clang on Cygwin handling

--- gnu/cc/clang.mk
+++ gnu/cc/clang.mk
@@ -1,15 +1,23 @@
 cc = clang -Wall -Werror -pedantic -c -pipe
 cflags =
-cdebug = -g -flto
-coptimization = -flto -O3
+cdebug = -g -flto -O0
+coptimization = -flto -O0
 cstd=c1x
 cppstd=c++0x
 
+ifeq ($(shell uname -s),CYGWIN_NT-5.2)
 lnk = llvm-ld -native
 lflags =
 ldebug =
 loptimization = -Xlinker=-static -Xlinker=-Wl,-s
-
+ar = llvm-ar
+$(warning Cygwin llvm-ld instead clang driver)
+else
+lnk = clang -pipe
+lflags =
+ldebug = -g -flto
+loptimization = -O3 -Wl,-s -flto
 ar = ar
+endif
 
 undefine toolchain
