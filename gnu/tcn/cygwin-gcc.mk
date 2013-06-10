5b35af750cd4f0417f34be9ec9da78b524e88495
mbakhterev <mob@k.imm.uran.ru>
2013-06-10 15:08:49 +0600

    Больше уровень отладки в cygwin-gcc

---
+++ gnu/tcn/cygwin-gcc.mk
@@ -0,0 +1,24 @@
+echo = echo -e
+
+cc = gcc -Wall -Werror -pedantic -c -pipe
+cflags =
+coptimization = -O3 -march=native -mtune=native
+cdebug = -g3
+cstd = c1x
+cppstd = c++0x
+dep = $(cc)
+
+lnk = gcc -pipe
+lflags =
+loptimization = -Wl,-s 
+ldebug = -g3
+
+ar = ar
+
+lex = flex -Caer -8 --yylineno --bison-locations
+
+yacc = bison -Wall -L C --locations
+
+# Переменные для подстройки конкретных целей 
+
+c99lexfix = -Wno-unused-but-set-variable
