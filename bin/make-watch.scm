69c9e22f25dbe0384887ad200b9412e36983df78
mbakhterev <mob@k.imm.uran.ru>
2018-03-09 22:54:01 +0500

    Цикл чтения данных от inotifywait

--- bin/make-watch.scm
+++ bin/make-watch.scm
@@ -1,3 +1,6 @@
+(use-modules (ice-9 popen))
+(use-modules (ice-9 rdelim))
+
 (define (collect-options arguments)
   (lambda () 
     (let lp ((mode #:none)
@@ -17,12 +20,26 @@
               ((#:dir) (lp mode (cdr arguments) suffixes (cons a directories)))
               (else (throw 'parse-options (format #f "no classifier for: ~a" a))))))))))
 
-(let* ((args (command-line))
+(define (watch-loop sfxs dirs)
+  (define (wrap s) (format #f " '~a'" s)) 
+
+  (define inotify-command
+    (apply string-append "inotifywait -rm -e close_write --format '%:e %f'" (map wrap dirs)))
+  
+  (define (trigger? str)
+    (not (and-map (lambda (x) (not (string-suffix? x str))) sfxs)))
+
+  (let ((p (open-input-pipe inotify-command)))
+    (let loop ((info (read-line p))
+               (thrd #f))
+      (if (not (eof-object? info))
+        (begin
+          (if (trigger? info) (begin (display info) (newline)) )
+          (loop (read-line p) thrd))))
+    (close-pipe p)))
+
+(display (let* ((args (command-line))
        (prog (car args)))
   (catch 'parse-options
-         (lambda ()
-           (call-with-values
-             (collect-options (cdr args))
-             (lambda (sfx dir) (format #t "sfx: ~s; dir: ~s~%" sfx dir))))
-         (lambda (key . args)
-           (format (current-error-port) "~a: ~a~%" prog (car args)))))
+         (lambda () (call-with-values (collect-options (cdr args)) watch-loop))
+         (lambda (key . args) (format (current-error-port) "~a: ~a~%" prog (car args))))))
