52f59094d16d5206a12591fd9f1b64e9a9a0e717
M.Bakhterev <mob@k.imm.uran.ru>
2018-09-28 22:36:23 +0500

    run-watch без массовых define-ов

--- bin/run-watch.scm
+++ bin/run-watch.scm
@@ -7,80 +7,84 @@
              (srfi srfi-41)
              (common))
 
+(define head car)
+(define tail cdr)
+
 (define (events opts)
-  (define to-read car)
-  (define to-write cdr) 
-
-  (define signal-pipe (pipe))
-  (define p-signal (to-read signal-pipe))
-  (define p-watch (apply open-pipe* OPEN_READ (inotify-command opts)))
-
-  (define (poll) (catch 'system-error
-                        (lambda () (car (select (list p-signal p-watch) '() '() #f)))
-                        (lambda err (if (= EINTR (system-error-errno err))
-                                      (poll)
-                                      (apply throw err)))))
-
-  (define-stream (events-stream ports)
-    (if (null? ports)
-      ; Если закончились готовые к чтению порты, вызываем poll и продолжаем
-      (events-stream (poll))
-      ; В противном случае, разбираемся, с первым портом в списке
-      (let ((p (car ports))
-            (rest (cdr ports)))
-        (cond ((equal? p-signal p) (let ((c (read-char p)))
-                                     (if (eq? #\D c)
-                                       (stream-cons #:done (events-stream rest))
-                                       (error "Unexpected signal char:" c))))
-              ((equal? p-watch p) (let ((info (read-line p)))
-                                    (if (eof-object? info)
-                                      (begin
-                                        (close-pipe p)
-                                        stream-null)
-                                      (let ((t? (trigger? opts info)))
-                                        (dump-error "~a: ~a~%" info (if t? "triggered" "skipped"))
-                                        (if t?
-                                          (stream-cons #:triggered (events-stream rest))
-                                          (events-stream rest))))))
-              (else (error "Unexpected port:" p))))))
-
-  (sigaction SIGCHLD
-             (let ((p (to-write signal-pipe))) (lambda (n) (write-char #\D p) (force-output p)))
-             SA_NOCLDSTOP)
-  
-  (events-stream '()))
+  (let ((read-end car)
+        (write-end cdr)
+        (signal-pipe (pipe))
+        (p-watch (apply open-pipe* OPEN_READ (inotify-command opts))))
+    (letrec ((p-signal (read-end signal-pipe))
+             (poll (let ((l (list p-signal p-watch)))
+                     (lambda () (catch 'system-error
+                                       (lambda () (head (select l '() '() #f)))
+                                       ; Если select прерван сигналом, то повторяем
+                                       ; вызов
+                                       (lambda er (if (= EINTR (system-error-errno er))
+                                                    (poll)
+                                                    (apply throw er))))))))
+      (sigaction SIGCHLD
+                 (let ((p (write-end signal-pipe)))
+                   (lambda (n) (write-char #\D p) (force-output p)))
+                 SA_NOCLDSTOP)
+      (stream-let loop ((ports '()))
+        (if (null? ports)
+          ; Если нет готовых к чтению портов, вызываем poll и продолжаем цикл
+          (loop (poll))
+          ; Иначе разбираемся с первым портом p в списке 
+          (let ((p (head ports))
+                (r (tail ports)))
+            (cond ((equal? p-signal p) (let ((c (read-char p)))
+                                         (if (eq? #\D c)
+                                           (stream-cons #:done (loop r))
+                                           (error "Unexpected signal char:" c))))
+                  ((equal? p-watch p) (let ((info (read-line p)))
+                                        (if (eof-object? info)
+                                          (begin (close-pipe p) stream-null)
+                                          (let ((t? (trigger? opts info)))
+                                            (dump-error "~a: ~a~%"
+                                                        info
+                                                        (if t? "triggered" "skipped"))
+                                            (if t?
+                                              (stream-cons #:triggered (loop r))
+                                              (loop r))))))
+                  (else (error "Unexpected port:" p)))))))))
 
 (define (main-loop opts s)
-  (define cmdline (options:command opts))
+  (let* ((fork-cmd (lambda () (let ((cl (options:command opts))
+                                    (p (primitive-fork)))
+                                (if (not (zero? p)) p (apply execlp (head cl) cl)))))
+         ; Структура состояния свёртки -- пара (pid есть-неучтённая-работа?)
+         (pid car)
+         (job? cdr)
+         (proc (lambda (st e)
+                 (case e
+                   ; Завершился дочерний процесс
+                   ((#:done) (if (not (= (pid st)
+                                         (pid (waitpid WAIT_ANY WNOHANG))))
+                               ; Это не рабочий процесс, менять нечего
+                               st
+                               ; Завершился рабочий процесс. Его нужно
+                               ; повторно запустить, если есть новая работа.
+                               ; Флаг о наличии работы надо сбросить. Если
+                               ; работ нет, ничего не запускаем, аннулируем pid
+                               ; в состоянии
+                               (cons (if (job? st) (fork-cmd) 0) #f)))
+                   ((#:triggered) (if (not (zero? (pid st)))
+                                    ; Не обнаружено завершение рабочего
+                                    ; процесса, запоминаем что есть работа
+                                    (cons (pid st) #t)
+                                    ; Иначе запускаем процесс, отмечая, что
+                                    ; работы учтены
+                                    (cons (fork-cmd) #f)))
+                   (else (error "Unexpected event:" e)))))
+         (final (stream-fold proc '(0 . #f) s)))
+    ; Если в конце оказался запущенным некоторый процесс, нужно дождаться его
+    ; завершения
+    (when (not (zero? (pid final)))
+      (waitpid (pid final)))))
 
-  (define (run-cmd) (let ((p (primitive-fork)))
-                      (if (not (zero? p)) p (apply execlp (car cmdline) cmdline))))
-
-  ; Состояние -- это пара (pid есть-ли-работа?)
-  (define pid car)
-  (define job? cdr)
-
-  (define (step st e)
-    (case e
-      ; Завершился некоторый дочерний процесс
-      ((#:done) (if (not (= (pid st)
-                            (pid (waitpid WAIT_ANY WNOHANG))))
-                  ; Если это не рабочий процесс, то ничего не меняется
-                  st
-                  ; Иначе, если есть некоторая работа, запускаем новый рабочий
-                  ; процесс, сбрасывая флаг о наличии работ. Если работы никакой
-                  ; нет, то обнуляем в состоянии информацию о текущем процессе
-                  (cons (if (job? st) (run-cmd) 0) #f)))
-      ; Появилась некоторая работа
-      ((#:triggered) (if (not (= 0 (pid st)))
-                       ; Если рабочий процесс не завершён, то запоминаем, что у
-                       ; нас есть новая работа
-                       (cons (pid st) #t)
-                       ; Если завершён, то нужно его запустить
-                       (cons (run-cmd) #f)))
-      (else (error "Unexpected event:" e)))) 
-
-  (stream-fold step '(0 . #f) s))
-
-(let ((opts (collect-options (cdr (command-line)))))
+(let ((opts (collect-options (tail (command-line)))))
+  (tune-env! opts)
   (main-loop opts (events opts)))
