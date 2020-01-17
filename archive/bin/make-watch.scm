749126e5e6ed4f53f5d6429f9dc69089fb60888d
mbakhterev <mob@k.imm.uran.ru>
2020-01-17 16:11:48 +0500

    Небольшая уборка

---
+++ archive/bin/make-watch.scm
@@ -0,0 +1,141 @@
+(use-modules (ice-9 popen)
+             (ice-9 rdelim)
+             (ice-9 atomic)
+             (ice-9 threads)
+             (srfi srfi-9 gnu))
+
+(define-immutable-record-type Options
+  (options suffixes directories target toolchain debug?)
+  options?
+  (suffixes options:suffixes)
+  (directories options:directories)
+  (target options:target)
+  (toolchain options:toolchain)
+  (debug? options:debug?))
+
+(define (collect-options arguments)
+  (lambda () 
+    (let lp ((mode #:none)
+             (arguments arguments)
+             (opts (options '() '() "" "" #f)))
+      (if (null? arguments)
+        (if (not (or (null? (options:suffixes opts))
+                     (null? (options:directories opts))
+                     (string-null? (options:target opts))))
+          opts
+          (throw 'parse-options (format #f "not enough options to run: ~a" opts)))
+        (let ((a (car arguments)))
+          (if (eq? #\- (string-ref a 0))
+            (cond
+              ((string= "-s" a) (lp #:sfx (cdr arguments) opts))
+              ((string= "-d" a) (lp #:dir (cdr arguments) opts))
+              ((string= "-D" a) (lp #:none (cdr arguments) (set-field opts (options:debug?) #t)))
+
+              ((string= "-B" a) (if (string-null? (options:target opts))
+                                  (lp #:tgt (cdr arguments) opts)
+                                  (throw 'parse-options
+                                         (format #f "build dir is already specified: ~a"
+                                                 (options:target opts)))))
+
+              ((string= "-T" a) (if (string-null? (options:toolchain opts))
+                                  (lp #:tcn (cdr arguments) opts)
+                                  (throw 'parse-options
+                                         (format #f "toolchain is already specified: ~a"
+                                                 (options:toolchain opts)))))
+
+              (else (throw 'parse-options (format #f "unknown option: ~a" a))))
+            (case mode
+              ((#:sfx) (lp mode
+                           (cdr arguments)
+                           (set-field opts (options:suffixes) (cons a (options:suffixes opts)))))
+               
+              ((#:dir) (lp mode
+                           (cdr arguments)
+                           (set-field opts (options:directories) (cons a (options:directories opts)))))
+
+              ((#:tcn) (lp #:none (cdr arguments) (set-field opts (options:toolchain) a)))
+              ((#:tgt) (lp #:none (cdr arguments) (set-field opts (options:target) a)))
+
+              (else (throw 'parse-options (format #f "no classifier for: ~a" a))))))))))
+
+(define (watch-loop opts)
+  (define sfxs (options:suffixes opts))
+  (define dirs (options:directories opts))
+
+  (define (wrap s) (format #f " '~a'" s)) 
+
+  (define inotify-command
+    (apply string-append "inotifywait -rm -e close_write --format '%:e %f'" (map wrap dirs)))
+
+  (define make-command
+    (string-append
+      (let ((v (options:toolchain opts))) (if (null? v) "" (format #f "TCN='~a' " v)))
+      (let ((v (options:target opts))) (if (null? v) "" (format #f "BDIR='~a' " v)))
+      "make -r"))
+  
+  (define (trigger? str)
+    (not (and-map (lambda (x) (not (string-suffix? x str))) sfxs)))
+
+  ; Протокол простой. Некий аналог светофора на трёх состояниях: nothing-to-do <
+  ; accepted < need-to-remake. Основной поток, следящий за сообщениями от
+  ; inotifywait тянет значения вверх, future, в котором запускается make тянет
+  ; значения вниз. Более подробно в заметке Сб мар 10 20:47:31 +05 2018
+
+  (define synchro (make-atomic-box #:nothing-to-do))
+
+  (define (remaking-loop)
+    ; Убеждаемся сначала, что протокол соблюдается, вначале этого цикла должен
+    ; быть запрос на пересборку
+    (if (not (eq? #:need-to-remake (atomic-box-ref synchro)))
+      (error "protocol violation: expecting #:need-to-remake"))
+
+    ; Со стороны основного потока #:need-to-remake не может изменится ни на что,
+    ; поэтому можно без cas-операции сбросить его в #:accepted и выполнить одну
+    ; итерацию сборки
+    (atomic-box-set! synchro #:accepted)
+    ; (system "make -r BDIR=/tmp/sci TCN=tex")
+    (system make-command)
+
+    ; Далее нужно действовать осторожнее. accepted может быть переведён снова в
+    ; need-to-remake, поэтому меняем через cas
+    (let ((v (atomic-box-compare-and-swap! synchro #:accepted #:nothing-to-do)))
+      ; Если предыдущее значение было #:accepted операция удалась, и можно
+      ; завершать работу. Иначе необходимо повторить цикл
+      (if (not (eq? #:accepted v)) (remaking-loop))))
+
+  (let ((p (open-input-pipe inotify-command)))
+    (let loop ((info (read-line p)))
+      (if (not (eof-object? info))
+        (begin
+          (format (current-error-port) "~a: ~a~%" info (if (trigger? info) "triggered" "skipping"))
+          (if (trigger? info)
+            ; Необходимо обеспечить перезапуск make. Анализируем состояние
+            (case (atomic-box-ref synchro)
+              ; Future не подхватит уже ничего, поэтому нужно запускать новое
+              ((#:nothing-to-do)
+               (atomic-box-set! synchro #:need-to-remake)
+               (call-with-new-thread remaking-loop))
+
+              ; Флаг уже установлен, и future должно это заметить, нечего делать
+              ((#:need-to-remake) '())
+
+              ; Состояние accepted может поменяться на nothing-to-do, поэтому
+              ; аккуратно меняем его через cas-гонку. Если гонка выиграна, то
+              ; future это заметит, если проиграна, то надо создать новое,
+              ; потому что текущее завершится. Убеждаемся перед этим в
+              ; корректности протокола
+              ((#:accepted) (let ((v (atomic-box-compare-and-swap! synchro #:accepted #:need-to-remake)))
+                              (if (not (eq? #:accepted v))
+                                (begin
+                                  (if (not (eq? #:nothing-to-do v))
+                                    (error "protocol violation: expecting #:nothing-to-do, got:" v))
+                                  (atomic-box-set! synchro #:need-to-remake)
+                                  (call-with-new-thread remaking-loop)))))))
+          (loop (read-line p)))))
+    (close-pipe p)))
+
+(let* ((args (command-line))
+       (prog (car args)))
+  (catch 'parse-options
+         (lambda () (call-with-values (collect-options (cdr args)) watch-loop))
+         (lambda (key . args) (format (current-error-port) "~a: ~a~%" prog (car args)))))
--- bin/make-watch.scm
+++ bin/make-watch.scm
@@ -1,141 +1,90 @@
+; Реализация на основе классических POSIX-механизмов
+
+(add-to-load-path (let ((fn (current-filename))) (if (string? fn) (dirname fn) ".")))
+
 (use-modules (ice-9 popen)
              (ice-9 rdelim)
-             (ice-9 atomic)
-             (ice-9 threads)
-             (srfi srfi-9 gnu))
+             (srfi srfi-41)
+             (common))
 
-(define-immutable-record-type Options
-  (options suffixes directories target toolchain debug?)
-  options?
-  (suffixes options:suffixes)
-  (directories options:directories)
-  (target options:target)
-  (toolchain options:toolchain)
-  (debug? options:debug?))
+(define head car)
+(define tail cdr)
 
-(define (collect-options arguments)
-  (lambda () 
-    (let lp ((mode #:none)
-             (arguments arguments)
-             (opts (options '() '() "" "" #f)))
-      (if (null? arguments)
-        (if (not (or (null? (options:suffixes opts))
-                     (null? (options:directories opts))
-                     (string-null? (options:target opts))))
-          opts
-          (throw 'parse-options (format #f "not enough options to run: ~a" opts)))
-        (let ((a (car arguments)))
-          (if (eq? #\- (string-ref a 0))
-            (cond
-              ((string= "-s" a) (lp #:sfx (cdr arguments) opts))
-              ((string= "-d" a) (lp #:dir (cdr arguments) opts))
-              ((string= "-D" a) (lp #:none (cdr arguments) (set-field opts (options:debug?) #t)))
+(define (events opts)
+  (let ((read-end car)
+        (write-end cdr)
+        (signal-pipe (pipe))
+        (p-watch (apply open-pipe* OPEN_READ (inotify-command opts))))
+    (letrec ((p-signal (read-end signal-pipe))
+             (poll (let ((l (list p-signal p-watch)))
+                     (lambda () (catch 'system-error
+                                       (lambda () (head (select l '() '() #f)))
+                                       ; Если select прерван сигналом, то повторяем
+                                       ; вызов
+                                       (lambda er (if (= EINTR (system-error-errno er))
+                                                    (poll)
+                                                    (apply throw er))))))))
+      (sigaction SIGCHLD
+                 (let ((p (write-end signal-pipe)))
+                   (lambda (n) (write-char #\D p) (force-output p)))
+                 SA_NOCLDSTOP)
+      (stream-let loop ((ports '()))
+        (if (null? ports)
+          ; Если нет готовых к чтению портов, вызываем poll и продолжаем цикл
+          (loop (poll))
+          ; Иначе разбираемся с первым портом p в списке 
+          (let ((p (head ports))
+                (r (tail ports)))
+            (cond ((equal? p-signal p) (let ((c (read-char p)))
+                                         (if (eq? #\D c)
+                                           (stream-cons #:done (loop r))
+                                           (error "Unexpected signal char:" c))))
+                  ((equal? p-watch p) (let ((info (read-line p)))
+                                        (if (eof-object? info)
+                                          (begin (close-pipe p) stream-null)
+                                          (let ((t? (trigger? opts info)))
+                                            (dump-error "~a: ~a~%"
+                                                        info
+                                                        (if t? "triggered" "skipped"))
+                                            (if t?
+                                              (stream-cons #:triggered (loop r))
+                                              (loop r))))))
+                  (else (error "Unexpected port:" p)))))))))
 
-              ((string= "-B" a) (if (string-null? (options:target opts))
-                                  (lp #:tgt (cdr arguments) opts)
-                                  (throw 'parse-options
-                                         (format #f "build dir is already specified: ~a"
-                                                 (options:target opts)))))
+(define (main-loop opts s)
+  (let* ((fork-cmd (lambda () (let ((cl (options:command opts))
+                                    (p (primitive-fork)))
+                                (if (not (zero? p)) p (apply execlp (head cl) cl)))))
+         ; Структура состояния свёртки -- пара (pid есть-неучтённая-работа?)
+         (pid car)
+         (job? cdr)
+         (proc (lambda (st e)
+                 (case e
+                   ; Завершился дочерний процесс
+                   ((#:done) (if (not (= (pid st)
+                                         (pid (waitpid WAIT_ANY WNOHANG))))
+                               ; Это не рабочий процесс, менять нечего
+                               st
+                               ; Завершился рабочий процесс. Его нужно
+                               ; повторно запустить, если есть новая работа.
+                               ; Флаг о наличии работы надо сбросить. Если
+                               ; работ нет, ничего не запускаем, аннулируем pid
+                               ; в состоянии
+                               (cons (if (job? st) (fork-cmd) 0) #f)))
+                   ((#:triggered) (if (not (zero? (pid st)))
+                                    ; Не обнаружено завершение рабочего
+                                    ; процесса, запоминаем что есть работа
+                                    (cons (pid st) #t)
+                                    ; Иначе запускаем процесс, отмечая, что
+                                    ; работы учтены
+                                    (cons (fork-cmd) #f)))
+                   (else (error "Unexpected event:" e)))))
+         (final (stream-fold proc '(0 . #f) s)))
+    ; Если в конце оказался запущенным некоторый процесс, нужно дождаться его
+    ; завершения
+    (when (not (zero? (pid final)))
+      (waitpid (pid final)))))
 
-              ((string= "-T" a) (if (string-null? (options:toolchain opts))
-                                  (lp #:tcn (cdr arguments) opts)
-                                  (throw 'parse-options
-                                         (format #f "toolchain is already specified: ~a"
-                                                 (options:toolchain opts)))))
-
-              (else (throw 'parse-options (format #f "unknown option: ~a" a))))
-            (case mode
-              ((#:sfx) (lp mode
-                           (cdr arguments)
-                           (set-field opts (options:suffixes) (cons a (options:suffixes opts)))))
-               
-              ((#:dir) (lp mode
-                           (cdr arguments)
-                           (set-field opts (options:directories) (cons a (options:directories opts)))))
-
-              ((#:tcn) (lp #:none (cdr arguments) (set-field opts (options:toolchain) a)))
-              ((#:tgt) (lp #:none (cdr arguments) (set-field opts (options:target) a)))
-
-              (else (throw 'parse-options (format #f "no classifier for: ~a" a))))))))))
-
-(define (watch-loop opts)
-  (define sfxs (options:suffixes opts))
-  (define dirs (options:directories opts))
-
-  (define (wrap s) (format #f " '~a'" s)) 
-
-  (define inotify-command
-    (apply string-append "inotifywait -rm -e close_write --format '%:e %f'" (map wrap dirs)))
-
-  (define make-command
-    (string-append
-      (let ((v (options:toolchain opts))) (if (null? v) "" (format #f "TCN='~a' " v)))
-      (let ((v (options:target opts))) (if (null? v) "" (format #f "BDIR='~a' " v)))
-      "make -r"))
-  
-  (define (trigger? str)
-    (not (and-map (lambda (x) (not (string-suffix? x str))) sfxs)))
-
-  ; Протокол простой. Некий аналог светофора на трёх состояниях: nothing-to-do <
-  ; accepted < need-to-remake. Основной поток, следящий за сообщениями от
-  ; inotifywait тянет значения вверх, future, в котором запускается make тянет
-  ; значения вниз. Более подробно в заметке Сб мар 10 20:47:31 +05 2018
-
-  (define synchro (make-atomic-box #:nothing-to-do))
-
-  (define (remaking-loop)
-    ; Убеждаемся сначала, что протокол соблюдается, вначале этого цикла должен
-    ; быть запрос на пересборку
-    (if (not (eq? #:need-to-remake (atomic-box-ref synchro)))
-      (error "protocol violation: expecting #:need-to-remake"))
-
-    ; Со стороны основного потока #:need-to-remake не может изменится ни на что,
-    ; поэтому можно без cas-операции сбросить его в #:accepted и выполнить одну
-    ; итерацию сборки
-    (atomic-box-set! synchro #:accepted)
-    ; (system "make -r BDIR=/tmp/sci TCN=tex")
-    (system make-command)
-
-    ; Далее нужно действовать осторожнее. accepted может быть переведён снова в
-    ; need-to-remake, поэтому меняем через cas
-    (let ((v (atomic-box-compare-and-swap! synchro #:accepted #:nothing-to-do)))
-      ; Если предыдущее значение было #:accepted операция удалась, и можно
-      ; завершать работу. Иначе необходимо повторить цикл
-      (if (not (eq? #:accepted v)) (remaking-loop))))
-
-  (let ((p (open-input-pipe inotify-command)))
-    (let loop ((info (read-line p)))
-      (if (not (eof-object? info))
-        (begin
-          (format (current-error-port) "~a: ~a~%" info (if (trigger? info) "triggered" "skipping"))
-          (if (trigger? info)
-            ; Необходимо обеспечить перезапуск make. Анализируем состояние
-            (case (atomic-box-ref synchro)
-              ; Future не подхватит уже ничего, поэтому нужно запускать новое
-              ((#:nothing-to-do)
-               (atomic-box-set! synchro #:need-to-remake)
-               (call-with-new-thread remaking-loop))
-
-              ; Флаг уже установлен, и future должно это заметить, нечего делать
-              ((#:need-to-remake) '())
-
-              ; Состояние accepted может поменяться на nothing-to-do, поэтому
-              ; аккуратно меняем его через cas-гонку. Если гонка выиграна, то
-              ; future это заметит, если проиграна, то надо создать новое,
-              ; потому что текущее завершится. Убеждаемся перед этим в
-              ; корректности протокола
-              ((#:accepted) (let ((v (atomic-box-compare-and-swap! synchro #:accepted #:need-to-remake)))
-                              (if (not (eq? #:accepted v))
-                                (begin
-                                  (if (not (eq? #:nothing-to-do v))
-                                    (error "protocol violation: expecting #:nothing-to-do, got:" v))
-                                  (atomic-box-set! synchro #:need-to-remake)
-                                  (call-with-new-thread remaking-loop)))))))
-          (loop (read-line p)))))
-    (close-pipe p)))
-
-(let* ((args (command-line))
-       (prog (car args)))
-  (catch 'parse-options
-         (lambda () (call-with-values (collect-options (cdr args)) watch-loop))
-         (lambda (key . args) (format (current-error-port) "~a: ~a~%" prog (car args)))))
+(let ((opts (collect-options (tail (command-line)))))
+  (tune-env! opts)
+  (main-loop opts (events opts)))
